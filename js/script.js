7147ea64c0b44bef71abba863c23bbac70869e52
DjB112 <david.j.bustamante@gmail.com>
2023-05-12 20:41:21 -0300

    sssssssssssssss

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let contenedor
 function cargar_imagen(valor){
     var x=document.getElementById(valor);
-    document.getElementById("monitor").src = x.src;
+    document.getElementById('monitor').src = x.src;
     contenedor = x.src;    
 }
 function siguiente_imagen(){
